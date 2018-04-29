24c9c227b489d9e963ec466914de91411ad33749
Josh Whitehouse <whitehouse.josh@gmail.com>
2018-04-29 17:21:38 -0400

    added sign up sheet
    sign up sheet addition

--- signUp.php
+++ signUp.php
@@ -2,6 +2,20 @@
 
 <?php include 'inc/nav.php';?>
 
+<p> Sign up for the newsletter below! </p>
+
+
+<div class="jumbotron paral paralsec3">
+  <h1 class="display-3"> Sign Up to learn More!</h1>
+    <p class="lead">
+      <form action="" method="post">
+        <input type="email" name="email" id="email" placeholder="email">
+        <input type="submit" value="Sign-Up">
+      </form>
+    </p>
+</div>
+
+
 
 
 <?php include 'inc/html-bottom/php';?>
